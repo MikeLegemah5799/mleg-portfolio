9805d60c0acbb8e951e24dbc8441b585cba592eb
Michael Legemah <michaellegemah@gmail.com>
2022-12-04 19:44:38 -0500

    configured tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      'blue': '#017AFF',
+      'purple': '#7A28CB',
+      ' green': '#39FF14',
+      'gray-dark': '#39393A',
+      'gray-light': '#E6E6E6',
+    },
     extend: {},
   },
   plugins: [],
