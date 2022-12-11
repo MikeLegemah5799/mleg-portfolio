4fd3c117024da4cadca719b72e9ea114dc1b6982
Michael Legemah <michaellegemah@gmail.com>
2022-12-11 15:51:42 -0500

    added custom Meta data component for SEO

---
+++ components/HeroSection/HeroSection.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import MikeMemoji from "../../public/static/mikeMemoji.png";
+
+
+const Hero = () => {
+    return (
+        <header role="banner" aria-label="Hero Section">
+        </header>
+    );
+};
+
+export default Hero;
