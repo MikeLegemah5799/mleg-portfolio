73db0d36dcc29e37de3c2707e2281f4b83b78eb9
Michael Legemah <michaellegemah@gmail.com>
2022-12-20 18:38:25 -0500

    created footer folder and file, moved navbar

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,14 @@
-import "../styles/globals.css";
-import { ThemeProvider } from "next-themes"
-import "@fortawesome/fontawesome-svg-core/styles.css"; // import Font Awesome CSS
-import { config } from "@fortawesome/fontawesome-svg-core";
+import '../styles/globals.css';
+import { ThemeProvider } from 'next-themes';
+import MainNav from '../components/MainNav/MainNav';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // import Font Awesome CSS
+import { config } from '@fortawesome/fontawesome-svg-core';
 config.autoAddCss = false;
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider defaultTheme="light" attribute="class">
+      <MainNav />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
-import MainNav from '../components/MainNav/MainNav';
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <MainNav />
     </div>
   )
 }
