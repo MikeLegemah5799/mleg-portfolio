2afc6ee410572fb4645f56003b4eb66856200c04
Michael Legemah <michaellegemah@gmail.com>
2022-12-20 23:01:06 -0500

    adjusted footer position

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -1,10 +1,43 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faLinkedinIn,
+    faGithub,
+    faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 
 const Footer = () => {
     return (
         <footer className="columns-2xs flex flex-wrap items-center justify-between p-3" aria-label="Footer">
             <p className="text-sm">&copy; {new Date().getFullYear()} Designed, developed, and maintained by Michael Legemah</p>
+            <ul className="inline-flex">
+                <li>
+                    <Link href="https://github.com/MikeLegemah5799" target="_blank" className="py-0.5 px-2">
+                        <FontAwesomeIcon
+                            icon={faGithub}
+                            style={{ fontSize: 18 }}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://www.linkedin.com/in/michaellegemah/" target="_blank" className="py-0.5 px-2">
+                        <FontAwesomeIcon
+                            icon={faLinkedinIn}
+                            style={{ fontSize: 18 }}
+                        />
+                    </Link>
+                </li>
+                <li>
+                    <Link href="https://twitter.com/MikeLegemah" target="_blank" className="py-0.5 px-2">
+                        <FontAwesomeIcon
+                            icon={faTwitter}
+                            style={{ fontSize: 18 }}
+                        />
+                    </Link>
+                </li>
+            </ul>
         </footer>
     );
 };
