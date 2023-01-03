3bcfbfb1b93a51d5ff3b1b9cd049d09b79cd0e51
Michael Legemah <michaellegemah@gmail.com>
2023-01-02 20:55:08 -0500

    added in testimonail component

---
+++ components/Testimonials/clients.js
@@ -0,0 +1,93 @@
+export const clients = [
+    {
+        image: '/static/yussufKhan.jpeg',
+        alt: 'Yussuf Khan Headshot',
+        name: 'Yussuf Khan',
+        title: 'Media Executive, Entrepreneur and Educator',
+        review: 'Michael is one of the most patient, knowledgeable, flexible and understanding people I have ever worked with. His ability to listen and resolve our site development issues got us through an extremely difficult time. Because of his talents, I still work with him to this day and have recommended Michael\'s services to many other businesses and individuals. I highly recommend working with Michael on your development needs.'
+    },
+    {
+        image: '/static/antonetteOrtiz.jpeg',
+        alt: 'Antonette Ortiz Headshot',
+        name: 'Antonette Ortiz',
+        title: 'Full Stack Software Engineer',
+        review: 'I had the opportunity to meet Michael through a mutual friend who knew I was looking to move into a more technical role. Upon meeting Michael, he was very warm, friendly, and dedicated. We had a resume review session and he was extremely helpful! He not only went over her comments and feedback but also gave me valuable advice on how to make changes. He spoke to me about what recruiters are looking for and how to make my resume stand out. He was able to answer my questions and give me constructive criticism which I greatly appreciate!'
+    },
+    {
+        image: '/static/jonathanBates.jpeg',
+        alt: 'Jonathan Bates Headshot',
+        name: 'Jonathan Bates',
+        title: 'Director of New Business Development, Madison Sqaure Garden Entertainment',
+        review: 'Michael is not only a very talented, adept, full stack developer, most importantly, he is a Man of integrity and a wonderful human being. I have had the chance to work directly with Michael on a weekly basis for a year and greatly appreciated his calm demeanor and logical approach. I recommend Michael very highly and will work with him again if opportunity ever permits.'
+    },
+    {
+        image: '/static/tonyLanda.jpeg',
+        alt: 'Tony Landa Headshot',
+        name: 'Tony Landa',
+        title: 'VP, Director of Technology, FCB Health',
+        review: 'I had the pleasure of working with Michael for a brief time on a high-intensity project. Michael was hired strictly to back-fill an Author position using Adobe Experience Manager (AEM) for an employee who departed with only a few days notice and little documentation. Not only did Michael figure out the (poorly maintained) system quickly, but we quickly learned his skills on Angular were of great service on other aspects of the project. Unfortunately, the project ended prematurely. Michael was no-drama, smart, and a great problem-solver. I fought to keep him, and I would happily work with him again.'
+    },
+    {
+        image: '/static/danieleWeinberger.jpeg',
+        alt: 'Daniele Weinberger Headshot',
+        name: 'Daniele Weinberger, CSM',
+        title: 'Senior Project Manager, Proximyl Health',
+        review: 'Mike is a consummate professional. He came on board with us for several months, picked up a very difficult AEM CMS interface very rapidly. He was a joy to work with, skilled, extremely responsive, proactive, friendly, supportive and solution oriented. From a project management perspective an absolute joy to have on the team. I miss working with him!'
+    },
+    {
+        image: '/static/scottKogos.jpeg',
+        alt: 'Scott Kogos Headshot',
+        name: 'Scott Kogos',
+        title: 'SVP, Director of Project Management Operations, Doner',
+        review: 'Mike is a consummate professional. He came on board with us for several months, picked up a very difficult AEM CMS interface very rapidly. He was a joy to work with, skilled, extremely responsive, proactive, friendly, supportive and solution oriented. From a project management perspective an absolute joy to have on the team. I miss working with him!'
+    },
+    {
+        image: '/static/alla.jpeg',
+        alt: 'Alla Gringaus Headshot',
+        name: 'Alla Gringaus',
+        title: 'Global Web Performance Optimization Lead, The Estée Lauder Companies Inc.',
+        review: 'Mike has a very solid front-end development experience in a scrum/agile development environment, with deep expertise in Wordpress and Shopify. He is adept at creating customer-focused, mobile-responsive UI with UX in mind. Self-directed and comfortable working on a small team with significant responsibility, Mike is a reliable, polite, kind and humble guy everyone wants to have on their side. It was a real pleasure to work with Mike.'
+    },
+    {
+        image: '/static/moAkram.jpeg',
+        alt: 'Mo Akram Headshot',
+        name: 'Mo Akram',
+        title: 'VP of Recruiting, Petfolk',
+        review: 'Michael played an integral part in the growth of Robert Half Technology\'s Salaried Professional Services. During his tenure with Robert Half Technology\'s Salaried Professional Services, Michael partnered with multiple clients, leading their Web & Creative Development initiatives. Michael collaborated with senior leadership & c-level executives to identify & translate specific needs bringing them to life, in addition to being a trusted adviser on overall direction of the engagements. Michael always stayed one step ahead of tech trends and best practices. Being a motivated team player and leader, Michael has a keen eye for the bigger picture. Throughout his engagements, Michael was highly recommended for his Full Stack Development, Front End Development, and creative eye for design. Michael is a valuable asset to any organization.'
+    },
+    {
+        image: '/static/andrew.jpeg',
+        alt: 'Andrew Hennessy Headshot',
+        name: 'Andrew Hennessy',
+        title: 'Technical Talent Acquisition Partner, Zendesk',
+        review: 'Michael was a member of our full time consultancy team at Robert Half who was incredibly dependable and consistent with his work. I could count on Michael to be readily available for projects or conversations with little to no notice. He is incredibly personable which made working with him very enjoyable.'
+    },
+    {
+        image: '/static/mary.jpeg',
+        alt: 'Mary Pinto Meyer Headshot',
+        name: 'Mary Pinto Meyer',
+        title: 'Director Human Resources Business Partner - Northeast, NFP',
+        review: 'Michael started a temporary assignment with our organization a few months ago. It started as a few week assignment, and has continued to be extended because of his great work. He is professional and has a wonderful demeanor that works well with clients.'
+    },
+    {
+        image: '/static/debra.jpeg',
+        alt: 'Debra Reiss Headshot',
+        name: 'Debra Reiss',
+        title: 'Psychotherapist',
+        review: 'Michael is an absolute pleasure to work with. He helped me with my existing website fixing a problem promptly, expertly, and graciously. He was able to diagnose a problem no one else seemed to understand, he took initiative, solved the problem, and had me up and running quickly. He\'s also a great communicator, and as a non-tech person I was able to understand all his explanations and instructions very easily. I am no longer afraid of web-based problems now that I have Michael\'s help. He is my new go-to-guy and I look forward to our continued relationship. I recommend Michael highly.'
+    },
+    {
+        image: '/static/chuck.jpeg',
+        alt: 'Chuck Killorin Headshot',
+        name: 'Chuck Killorin',
+        title: 'Executive Creative Director, Millennium Communications',
+        review: 'Michael worked with us on a contract basis and in his time with us he was a great part of the team. Michael has a head for front-end development, eagerly jumping in to projects, learning new technologies, and often finding things others missed. Michael\'s also a gentlemen with always something positive to say and an upbeat attitude.'
+    },
+    {
+        image: '/static/frank.jpeg',
+        alt: 'Frank Durante Headshot',
+        name: 'Frank Durante',
+        title: 'Internet Application Specialist, Nikon',
+        review: 'Michael is, by far, one of the most kind, passionate and loyal individual I have ever had the pleasure of working with. His work ethic is exceptional and respected by everyone in the company. I have never witnessed stress or frustration from Michael. Michael handles every task assigned with the proper care and attention you wish everyone would follow. I respect his constant appetite for knowledge as he regularly tunes his skills to stay up front in the pack of new technology. '
+    }
+];
