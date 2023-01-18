a523470f2bcbe5950e920f7eb751cde8958e37cf
Michael Legemah <michaellegemah@gmail.com>
2023-01-17 21:49:30 -0500

    adjusted SEO

---
+++ components/Timeline/Timeline.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { motion } from 'framer-motion';
+
+const Timeline = () => {
+    return (
+        <section className="mx-auto container py-24 px-6">
+        </section>
+    );
+}
+
+export default Timeline;
